c34e70b0d89f5741e3f39f080b21405e35c8aaa6
sterbye <simonak.damian123@gmail.com>
2024-05-30 18:48:14 +0200

    Adding packages, tailwind, edit icon and build layout with navigation

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,35 @@
-import Logo from "./_components/Logo";
-import Navigation from "./_components/Navigation";
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cain hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by the Wild Oasis</footer>
+      <body
+        className={`${josefin.className}relative flex flex-col bg-primary-950 text-gray-50 min-h-screen`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 ">
+          <main className="max-w-7xl  mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
