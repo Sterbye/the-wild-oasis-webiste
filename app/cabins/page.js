011aa6dc1b122a69f233d79393c17d56e9b95966
Damian Simonak <simonak.damian123@gmail.com>
2024-06-10 16:13:46 +0200

    Implementation Filter but still mising URL part

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,18 @@
 import CabinList from "@/app/_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
+//We do not need anymore this because of searchParams but leave it as it is
 export const revalidate = 3600;
 
 export const metadata = {
   title: "Cabin",
 };
 
-export default async function Page() {
+export default async function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,8 +26,12 @@ export default async function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
+
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
       <Suspense fallback={<Spinner />}>
-        <CabinList />
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
