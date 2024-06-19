31cd2dcd44c6a5cd453a19bcba1b668688d2c608
Damian Simonak <simonak.damian123@gmail.com>
2024-06-19 15:18:14 +0200

    Implementation createReservation

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -162,7 +162,7 @@ export async function createGuest(newGuest) {
   return data;
 }
 
-export async function createBooking(newBooking) {
+/*export async function createBooking(newBooking) {
   const { data, error } = await supabase
     .from("bookings")
     .insert([newBooking])
@@ -176,7 +176,7 @@ export async function createBooking(newBooking) {
   }
 
   return data;
-}
+}*/
 
 /////////////
 // UPDATE
